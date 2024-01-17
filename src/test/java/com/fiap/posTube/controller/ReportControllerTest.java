f4cccc63ab139219da980598b58951c818dbae68
Bruno Miguel Morais <bm.morais21@gmail.com>
2024-01-17 20:23:08 -0300

    Removendo annotation desnecessária

--- src/test/java/com/fiap/posTube/controller/ReportControllerTest.java
+++ src/test/java/com/fiap/posTube/controller/ReportControllerTest.java
@@ -1,21 +1,16 @@
 package com.fiap.posTube.controller;
 
-import com.fiap.posTube.PosTubeApplication;
 import com.fiap.posTube.service.ReportService;
 import com.fiap.posTube.useCase.DTO.ReportDTO;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @WebFluxTest(controllers = ReportController.class)
-@SpringBootTest(classes = PosTubeApplication.class)
 public class ReportControllerTest {
 
     @Autowired
